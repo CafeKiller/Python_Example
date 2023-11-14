00b42be240f99c24779339905ef699012b9fe1a0
CoffeeKiller <18320210361@163.com>
2023-11-14 23:09:11 +0800

    feat: [imageMS] init

---
+++ imageMS/main.py
@@ -0,0 +1,8 @@
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtGui import QBrush,QPixmap
+
+if __name__ == "__main__":
+    import sys
+    app = Qt
