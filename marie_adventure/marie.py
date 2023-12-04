9297a14bb356c117a26b226432c899027e2796a3
CoffeeKiller <18320210361@163.com>
2023-12-04 22:40:03 +0800

    feat: [marie_adventure] project init

---
+++ marie_adventure/marie.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+SCREEN_WIDTH = 822
+SCREEN_HEIGHT = 199
+FPS = 30
+
+
+# 游戏主函数
+def mainGame():
+    pass
+
+
+# 游戏结束函数
+def gameOver():
+    pass
+
+
+if __name__ == "__main__":
+    mainGame()
