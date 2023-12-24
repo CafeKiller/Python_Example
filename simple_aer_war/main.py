60de997759ab536692b289b701503391af863cf4
CoffeeKiller <18320210361@163.com>
2023-12-24 18:41:50 +0800

    feat: 添加移动判定

--- simple_aer_war/main.py
+++ simple_aer_war/main.py
@@ -1,6 +1,9 @@
 import pygame
 
 
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 800
+
 # 子弹类
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, bullet_img, init_pos):
@@ -26,3 +29,38 @@ class Player(pygame.sprite.Sprite):
         self.bullets = pygame.sprite.Group()
         self.img_index = 0
         self.is_hit = False
+
+    # 发射子弹
+    def shoot(self, bullet_img):
+        bullet = Bullet(bullet_img, self.rect.midtop)
+        self.bullets.add(bullet)
+
+    # 向上移动(需要判定边界)
+    def moveUp(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        else:
+            self.rect.top -= self.speed
+
+    # 向下移动(需要判定边界)
+    def moveDown(self):
+        if self.rect.top >= SCREEN_HEIGHT - self.rect.height:
+            self.rect.top = SCREEN_HEIGHT - self.rect.height
+        else:
+            self.rect.top += self.speed
+
+    # 向左移动
+    def moveLeft(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        else:
+            self.rect.left -= self.speed
+
+    # 向右移动
+    def moveRight(self):
+        if self.rect.left >= SCREEN_WIDTH - self.rect.width:
+            self.rect.left = SCREEN_WIDTH - self.rect.width
+        else:
+            self.rect.left += self.speed
+
+
